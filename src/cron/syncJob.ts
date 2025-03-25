21ef5db1370e220e670f52b72720959f8f2990cb
JinDevWS <jindev2024@gmail.com>
2025-03-25 19:05:35 +0900

    feat: cron 동기화 로그 출력시 한국 현재시간 기준으로 표기

--- src/cron/syncJob.ts
+++ src/cron/syncJob.ts
@@ -1,15 +1,18 @@
 import cron from 'node-cron';
 import { syncFlights } from '@/services/flightService';
+import { DateTime } from 'luxon';
 
 export const startFlightSyncJob = () => {
   // 매 1분마다 실행
   cron.schedule('* * * * *', async () => {
-    console.log('[Cron] 항공편 동기화 시작');
+    const dateTime = DateTime.now().setZone('Asia/Seoul').toFormat('yyyy-MM-dd HH:mm:ss');
+
+    console.log('[Cron] 항공편 동기화 시작: ', dateTime);
     try {
       await syncFlights();
-      console.log('[Cron] 항공편 동기화 완료');
+      console.log('[Cron] 항공편 동기화 완료: ', dateTime);
     } catch (error) {
-      console.error('[Cron] 동기화 실패:', error);
+      console.error('[Cron] 동기화 실패:', error, dateTime);
     }
   });
 };
